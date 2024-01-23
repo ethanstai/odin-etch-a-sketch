eaf53a9f00b0dc0abda3081d0eccb67d8f66af4e
Ethan Tai <152665898+ethanstai@users.noreply.github.com>
2024-01-23 15:39:50 -0800

    Nest a for loop into the original to create separate containers for each row

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const container = document.querySelector('#container');
 
-for (let i = 0; i <= (16*16); i++) {
-    let div = document.createElement('div');
-    container.appendChild(div);
+for (let i = 0; i <= 16; i++) {
+    let row = document.createElement('div');
+    row.setAttribute('id', 'row');
+    container.appendChild(row);
+    for (let j = 0; j <= 16; j++) {
+        let div = document.createElement('div');
+        div.setAttribute('id', 'square');
+        row.appendChild(div);
+    }
 }
